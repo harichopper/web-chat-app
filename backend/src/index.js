8e62ca4861402b61224d3a96466a4ba3a84ca8de
HARIHARAN R <144225049+harichopper@users.noreply.github.com>
2024-12-10 16:44:00 +0530

    Add files via upload

---
+++ backend/src/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Adjust based on your frontend port
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Basic route for testing
+app.get("/", (req, res) => {
+  console.log("Root route hit");
+  res.send("Hello, welcome to the backend server!");
+});
+
+// Static file serving for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Start the server
+server.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB(); // Connect to MongoDB
+});
